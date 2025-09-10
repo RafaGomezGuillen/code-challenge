79587dc8ab579b0ab7ad5ef966f5906d87ce4d74
Rafa Gómez <rafagomezguillen03@gmail.com>
2025-09-10 08:48:58 +0100

    Fix: Execute seeds more than one time

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,12 +1,12 @@
 # Products
-Product.create!(code: "GR1", name: "Green Tea", price: 3.11)
-Product.create!(code: "SR1", name: "Strawberries", price: 5.00)
-Product.create!(code: "CF1", name: "Coffee", price: 11.23)
+Product.find_or_create_by!(code: "GR1") { |p| p.name = "Green Tea"; p.price = 3.11 }
+Product.find_or_create_by!(code: "SR1") { |p| p.name = "Strawberries"; p.price = 5.00 }
+Product.find_or_create_by!(code: "CF1") { |p| p.name = "Coffee"; p.price = 11.23 }
 
 # Pricing Rules
-BogoRule.create!(sku: "GR1")
-BulkPriceRule.create!(sku: "SR1", threshold: 3, new_price: 4.5)
-PercentageBulkRule.create!(sku: "CF1", threshold: 3, factor: 2.0/3.0)
+BogoRule.find_or_create_by!(sku: "GR1")
+BulkPriceRule.find_or_create_by!(sku: "SR1", threshold: 3, new_price: 4.5)
+PercentageBulkRule.find_or_create_by!(sku: "CF1", threshold: 3, factor: 2.0/3.0)
 
 # Cart
-Cart.create!
+Cart.first_or_create!
